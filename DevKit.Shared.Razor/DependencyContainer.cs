ffb25f46aac2e83c0a4e36d19e321863be51bdf8
David Vázquez Palestino <76629621+davidvazquezpalestino@users.noreply.github.com>
2025-09-02 00:11:08 -0600

    Alertas

--- DevKit.Shared.Razor/DependencyContainer.cs
+++ DevKit.Shared.Razor/DependencyContainer.cs
@@ -1,8 +1,5 @@
 namespace DevKit.Shared.Razor;
 
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.DependencyInjection.Extensions;
-
 /// <summary>
 /// Proporciona métodos de extensión para registrar servicios de componentes de UI en el contenedor de dependencias.
 /// </summary>
@@ -11,7 +8,7 @@ public static class DependencyContainer
     /// <summary>
     /// Registra los servicios de componentes de UI personalizados en el contenedor de dependencias.
     /// </summary>
-    public static IServiceCollection AddComponentsCustom(
+    public static IServiceCollection AddSweetAlert(
         this IServiceCollection services,
         ServiceLifetime lifetime = ServiceLifetime.Scoped)
     {
@@ -21,10 +18,10 @@ public static class DependencyContainer
         }
 
         services.TryAdd(new ServiceDescriptor(
-            typeof(ISweetAlert), 
-            typeof(SweetAlert), 
+            typeof(ISweetAlert),
+            typeof(SweetAlert),
             lifetime));
-            
+
         return services;
     }
 }
--- DevKit.Shared.Razor/GlobalUsings.cs
+++ DevKit.Shared.Razor/GlobalUsings.cs
@@ -6,3 +6,5 @@ global using Microsoft.AspNetCore.Components.Web;
 global using Microsoft.JSInterop;
 global using System;
 global using System.Reflection;
+global using Microsoft.Extensions.DependencyInjection;
+global using Microsoft.Extensions.DependencyInjection.Extensions;
